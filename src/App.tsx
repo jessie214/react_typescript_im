f7bdb3bcc037f38170ad7da881d221f65ff2027b
JessieYan214 <jessieyan214@gmail.com>
2022-10-15 16:02:50 +1300

    fix app text

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React CC
         </a>
       </header>
     </div>   
