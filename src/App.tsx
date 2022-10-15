582028fc1c53eef2d0d3faafbf3fdb94a87f547f
JessieYan214 <jessieyan214@gmail.com>
2022-10-15 22:04:18 +1300

    add router, set router for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import styles from './App.module.css';
 
-function  IsMobile (){
-  let plat = navigator.userAgent.match(  
-/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-  );
-  if (plat) return 'yse';
-  return 'no';
-}
-let isMobile = IsMobile();
+import { MobilePage } from './pages/MobilePage';
+const {BrowserRouter,Route,} = require('react-router-dom');
+// function  IsMobile (){
+//   let plat = navigator.userAgent.match(  
+// /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+//   );
+//   if (plat) return 'yse';
+//   return 'no';
+// }
+// let isMobile = IsMobile();
 
 
 function App() {
   return (    
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <BrowserRouter>
+      <Route exact path="/" component={MobilePage} />
+      </BrowserRouter>
+
+
+      {/* <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         { isMobile==='yse'?'mobile':'pc'}
         <a
-          className="App-link"
+          className={styles.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React CC
         </a>
-      </header>
+      </header> */}
     </div>   
     
   );
